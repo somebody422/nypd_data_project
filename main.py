bafd2bc7086d9441d7c1d89f5d8279f2aa5e0153
unknown <shedin422@gmail.com>
2018-04-19 16:59:09 -0400

    Convert everything from string to float

--- main.py
+++ main.py
@@ -8,11 +8,35 @@ import matplotlib.pyplot as pyplot
 
 # Takes a string. Return true iff it is a number
 def isNumber(s):
+	# Special case: empty string is a string
+	if s == '':
+		return False
 	for c in s:
 		if c < '0' or c > '9':
 			if c != '.' and c != '-':
 				return False
 	return True
+
+
+# I'm borrowing from C. An "enum" here is an number representation
+#  of a string. As I go through I will be making sure EVERYTHING
+#  is a number, which will numpy much nicer
+class Enum:
+	def __init__(self):
+		self.next_index = 1
+		self.name_from_num = []
+		self.num_from_name = {}
+
+	def addValue(self, name):
+		self.name_from_num.append(name)
+		self.num_from_name[name] = self.next_index
+		self.next_index += 1
+
+	#def numFromName(self, name):
+	#	return self.num_from_name[name]
+	#def nameFromNum(self, num):
+	#	return self.name_from_num[num]
+
 
 
 if len(sys.argv) != 2:
@@ -44,18 +68,40 @@ print("\nIndexes:")
 print(index_of_attribute)
 
 records = []
-for line in input_file:
-	records.append(map(lambda x: x.strip(), line.split(',')))
+enums = [Enum() for _ in attributes]
+for strline in input_file:
+	fields = strline.split(',')
+	line = []
+	for i in range(len(fields)):
+		field = fields[i].strip()
+		#print("Field: " + str(field))
+		if isNumber(field):
+			line.append(float(field))
+		else:
+			if field not in enums[i].num_from_name:
+				enums[i].addValue(field)
+			line.append(enums[i].num_from_name[field])
+	records.append(line)
+	print(strline + "  --->  " + str(line))
+	#records.append(map(lambda x: x.strip(), line.split(',')))
 
+print("\nEnums:")
+print(enums)
 
 input_file.close()
-
+print("\nRecords:")
 print(records)
+
+
+
+
+
 
 # Keep in mind numpy arrays do not do mixed types by
 #  default!! This means that the records will be stored as strings
 print("Converting to np array..")
-records_matrix = np.matrix(records, dtype='bytes_')
+#records_matrix = np.matrix(records, dtype='bytes_')
+records_matrix = np.matrix(records)
 print("\nNormal np array:")
 print(records_matrix)
 print(type(records_matrix))
@@ -74,10 +120,6 @@ print(records_matrix_transposed)
 
 #gc.collect()
 
-
-
-
-
 # Plot the long/lat of the accidents
 
 print("\nX values are latitude:")
@@ -85,12 +127,11 @@ print(records_matrix_transposed[index_of_attribute["LONGITUDE"]])
 
 print("\nY values are longitude:")
 print(records_matrix_transposed[index_of_attribute["LATITUDE"]])
-figure, axes = pyplot.subplots(0, 0)
+figure, axes = pyplot.subplots(1, 1)
 axes.plot(
 	records_matrix_transposed[index_of_attribute["LONGITUDE"]],
-	records_matrix_transposed[index_of_attribute["LATITUDE"]],
-	linestyle = 'None',
-
+	records_matrix_transposed[index_of_attribute["LATITUDE"]]
+	#linestyle = 'None'
 )
 pyplot.show()
 
