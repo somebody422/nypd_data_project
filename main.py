9b21d13d9c9aa63fc13c30dd60964df8335ad145
unknown <shedin422@gmail.com>
2018-04-19 09:37:23 -0400

    starting to make a graph

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as pyplot
 #import gc
 
 
@@ -35,61 +37,64 @@ attributes_with_whitespace = input_file.readline().split(',')
 attributes = map(lambda x: x.strip(), attributes_with_whitespace)
 print(attributes)
 
+index_of_attribute = {}
+for i in range(len(attributes)):
+	index_of_attribute[attributes[i]] = i
+print("\nIndexes:")
+print(index_of_attribute)
+
 records = []
 for line in input_file:
 	records.append(map(lambda x: x.strip(), line.split(',')))
 
-#for record in records:
-#	print(len(record))
 
 input_file.close()
-
-#dont keep this
-for row in range(len(records)):
-	for col in range(len(records[row])):
-		if records[row][col].find('\"') >= 0:
-			records[row][col] = "butts"
-
-for record in records:
-	print(len(record))
-	for d in record:
-		if not ( type(d) is str):
-			print("ERROR: record is not string")
-			print(record)
-			print(d)
 
 print(records)
 
 # Keep in mind numpy arrays do not do mixed types by
 #  default!! This means that the records will be stored as strings
 print("Converting to np array..")
-records_np = np.matrix(records, dtype='bytes_')
+records_matrix = np.matrix(records, dtype='bytes_')
 print("\nNormal np array:")
-print(records_np)
-print(type(records_np))
-print(records_np.dtype)
+print(records_matrix)
+print(type(records_matrix))
+print(records_matrix.dtype)
 
-#print("Creating a transposed version..")
-#records_np_transposed = records_np.getT()
-#print("\nTransposed np array:")
-#print(records_np_transposed)
+print("Creating a transposed records_matrix..")
+records_matrix_transposed = records_matrix.getT()
+print("\nTransposed np array:")
+print(records_matrix_transposed)
 
 
-#cov_matrix = np.cov(records_np)
+#cov_matrix = np.cov(records_matrix, rowvar=False)
 #print("\nCovariance matrix:")
 #print(cov_matrix)
 
 
 #gc.collect()
 
-# Next step: Data cleaning!
-# Try creating several versions of "attributes" and "records". Each
-#  Will only have the attribtues we are interested in.
-#  Do this with references somehow? Don't really want multiple copies of 
-#  that large database..
 
 
 
 
 # Plot the long/lat of the accidents
+
+print("\nX values are latitude:")
+print(records_matrix_transposed[index_of_attribute["LONGITUDE"]])
+
+print("\nY values are longitude:")
+print(records_matrix_transposed[index_of_attribute["LATITUDE"]])
+figure, axes = pyplot.subplots(0, 0)
+axes.plot(
+	records_matrix_transposed[index_of_attribute["LONGITUDE"]],
+	records_matrix_transposed[index_of_attribute["LATITUDE"]],
+	linestyle = 'None',
+
+)
+pyplot.show()
+
+
+
+
 
